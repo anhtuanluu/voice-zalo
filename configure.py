32bfea4ccc98c2cf3dc4285bc946a08afb0777ef
Tuan <atuanbk58@gmail.com>
2020-11-03 19:53:45 +0700

    update

---
+++ configure.py
@@ -0,0 +1,15 @@
+# Wave path
+TRAIN_WAV_DIR = '/home/tuan/Documents/Train-Test-Data/dataset'
+
+# Feature path
+TRAIN_FEAT_DIR = '/home/tuan/Documents/Train-Test-Data/dataset'
+
+# Context window size
+NUM_WIN_SIZE = 200 #10
+
+# Settings for feature extraction
+USE_LOGSCALE = True
+USE_DELTA = False
+USE_SCALE = False
+SAMPLE_RATE = 16000
+FILTER_BANK = 40
---
+++ exportfeats.py
@@ -0,0 +1,31 @@
+import librosa
+import numpy as np
+from python_speech_features import fbank
+import configure as c
+from DB_wav_reader import read_DB_structure
+import pickle
+
+def normalize_frames(m,Scale=True):
+    if Scale:
+        return (m - np.mean(m, axis=0)) / (np.std(m, axis=0) + 2e-12)
+    else:
+        return (m - np.mean(m, axis=0))
+
+audiopath = c.TRAIN_WAV_DIR
+db = read_DB_structure(audiopath)
+
+feat_and_label={}
+
+print(db["filename"])
+for filename, label in zip(db["filename"], db["speaker_id"]):
+    print(filename)
+    print(label)
+    audio, sr = librosa.load(filename, sr=c.SAMPLE_RATE, mono=True)
+    filter_banks, energies = fbank(audio, samplerate=c.SAMPLE_RATE, nfilt=40, winlen=0.025)
+    filter_banks = 20 * np.log10(np.maximum(filter_banks,1e-5))
+    feature = normalize_frames(filter_banks, Scale=False)
+    feat_and_label['feat'] = feature
+    feat_and_label['label'] = label
+    save_file = filename[:-4] + ".p"
+    with open(save_file, "wb") as f:
+        pickle.dump(feat_and_label, f)
